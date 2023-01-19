356611b400207cc662716d367ab8f1ca8205570a
Mike Thomas <mikethomasmobile@gmail.com>
2023-01-19 16:36:02 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<div class="container-sm">
+    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><strong>Movie Tool</strong></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Genres</a>
+              <a class="nav-link" href="#">Random</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      
+      <div class="d-grid pt-2 gap-2 d-md-flex justify-content-md-end">
+        <button class="btn btn-primary" type="button">Add movie</button>
+      </div>
+    <div class="container">
+        <h4>My movies</h4>
+     
+        <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Movie name</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Aliens</td>
+            <td>Horror</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Ninth Gate</td>
+            <td>Thriller</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Willow</td>
+            <td>Fantasy</td>
+            <td>4</td>
+          </tr>
+        </tbody>
+     </table>
+    </div>
+
+</div>
